88aa9868d929b2a980624c1fffab9761f26605e2
giolga <giolga.kumi@gmail.com>
2024-09-19 17:57:57 +0400

    First Commit

---
+++ ShoppingListAPI/Models/ShoppingListContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShoppingListAPI.Models
+{
+    public class ShoppingListContext : DbContext
+    {
+        public ShoppingListContext(DbContextOptions<ShoppingListContext> options) : base(options) { }
+
+        public DbSet<Grocery> Groceries { get; set; }
+
+    }
+}
